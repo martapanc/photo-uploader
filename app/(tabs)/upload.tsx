a88de5bd12bf870e8558403fadda5182d5c464d6
martapanc <marta.panc@gmail.com>
2024-11-11 20:24:16 +0100

    Add simple image uploader form

--- app/(tabs)/upload.tsx
+++ app/(tabs)/upload.tsx
@@ -1,35 +1,89 @@
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { StyleSheet } from 'react-native';
+import {Button, Image, StyleSheet, TextInput} from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+
 
 import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import {ThemedText} from '@/components/ThemedText';
+import {ThemedView} from '@/components/ThemedView';
+import {useState} from "react";
 
-export default function TabTwoScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Upload</ThemedText>
+export default function UploadScreen() {
+    const [title, setTitle] = useState<string>('');
+    const [description, setDescription] = useState<string>('');
+    const [image, setImage] = useState<string>('');
 
-        <div className="flex flex-col">
+    const pickImage = async () => {
+        const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (!permissionResult.granted) {
+            alert('Permission to access files required');
+            return;
+        }
 
-        </div>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+        const result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            quality: 1
+        });
+
+        if (!result.canceled) {
+            setImage(result.assets[0].uri);
+        }
+    }
+    return (
+        <ParallaxScrollView
+            headerBackgroundColor={{light: '#D0D0D0', dark: '#353636'}}
+            headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage}/>}>
+            <ThemedView style={styles.titleContainer}>
+                <ThemedText type="title">Upload</ThemedText>
+
+                <ThemedView style={styles.formContainer}>
+                    <TextInput
+                        placeholder="Photo Title"
+                        value={title}
+                        onChangeText={setTitle}
+                        style={styles.input}
+                    />
+
+                    <TextInput
+                        placeholder="Description"
+                        value={description}
+                        onChangeText={setDescription}
+                        style={[styles.input, styles.descriptionInput]}
+                        multiline
+                    />
+
+                    <Button title={'Pick a photo'} onPress={pickImage} />
+                    {image && <Image source={{ uri: image }} style={styles.image} />}
+                </ThemedView>
+            </ThemedView>
+        </ParallaxScrollView>
+    );
 }
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
-  },
-  titleContainer: {
-    flexDirection: 'row',
-    gap: 8,
-  },
+    titleContainer: {
+        padding: 20,
+    },
+    formContainer: {
+        marginTop: 20,
+        gap: 10,
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        padding: 10,
+        borderRadius: 5,
+    },
+    descriptionInput: {
+        height: 100,
+    },
+    image: {
+        width: 100,
+        height: 100,
+        marginTop: 10,
+    },
+    headerImage: {
+        alignSelf: 'center',
+    },
 });
