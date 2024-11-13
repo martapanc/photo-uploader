6af8f03c28ce2eba24ee28399055ae239f212524
martapanc <marta.panc@gmail.com>
2024-11-13 19:18:44 +0100

    Switch to yarn; install Cloudinary

--- app/(tabs)/upload.tsx
+++ app/(tabs)/upload.tsx
@@ -1,8 +1,6 @@
 import Ionicons from '@expo/vector-icons/Ionicons';
 import {Button, Image, StyleSheet, TextInput} from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
-
-
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import {ThemedText} from '@/components/ThemedText';
 import {ThemedView} from '@/components/ThemedView';
