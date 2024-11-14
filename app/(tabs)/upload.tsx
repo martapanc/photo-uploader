2c647a5d85324c6add5ea5e9048cee11b3519e71
martapanc <marta.panc@gmail.com>
2024-11-14 20:01:47 +0100

    Upload photo to Cloudinary

--- app/(tabs)/upload.tsx
+++ app/(tabs)/upload.tsx
@@ -6,10 +6,17 @@ import {ThemedText} from '@/components/ThemedText';
 import {ThemedView} from '@/components/ThemedView';
 import {useState} from "react";
 
+const config = {
+    cloud_name: 'dwrurydlt',
+    api_key: '135348826679948',
+    api_secret: 'Nelsrm6BkHbwHNERV4rwADD5gMQ',
+}
+
 export default function UploadScreen() {
     const [title, setTitle] = useState<string>('');
     const [description, setDescription] = useState<string>('');
     const [image, setImage] = useState<string>('');
+    const [uploading, setUploading] = useState<boolean>(false);
 
     const pickImage = async () => {
         const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
@@ -26,8 +33,50 @@ export default function UploadScreen() {
 
         if (!result.canceled) {
             setImage(result.assets[0].uri);
+            alert(JSON.stringify(result.assets[0]));
         }
     }
+
+    const handleUpload = async () => {
+        if (!title || !description || !image) {
+            alert('Please fill in all fields');
+            return;
+        }
+
+        setUploading(true);
+
+        const formData = new FormData();
+        formData.append('file', {
+            uri: image,
+            type: 'image/jpeg',
+            name: `${title}.webp`
+        } as any);
+        formData.append('upload_preset', 'Convert to Webp');
+        formData.append('folder', 'Food');
+        formData.append('context', `caption=${title}|alt=${description}`);
+
+        try {
+            const response = await fetch(`https://api.cloudinary.com/v1_1/${config.cloud_name}/image/upload`,
+                {
+                    method: 'POST',
+                    body: formData,
+                },
+            )
+
+            const data = await response.json();
+
+            if (data.error) {
+                alert('Upload failed: ' + data.error.message);
+            } else {
+                alert('Upload successful!');
+            }
+        } catch (error) {
+
+        } finally {
+            setUploading(false);
+        }
+    }
+
     return (
         <ParallaxScrollView
             headerBackgroundColor={{light: '#D0D0D0', dark: '#353636'}}
@@ -53,6 +102,8 @@ export default function UploadScreen() {
 
                     <Button title={'Pick a photo'} onPress={pickImage} />
                     {image && <Image source={{ uri: image }} style={styles.image} />}
+
+                    <Button title={uploading ? 'Uploading...' : 'Upload'} onPress={handleUpload} />
                 </ThemedView>
             </ThemedView>
         </ParallaxScrollView>
